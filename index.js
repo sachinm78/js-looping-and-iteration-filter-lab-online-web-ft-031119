abf2e856adb7e4e9e9d46d14bc55956d4413cabb
Sachin Mahajan <sachinm78@gmail.com>
2019-06-12 16:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file
+
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
