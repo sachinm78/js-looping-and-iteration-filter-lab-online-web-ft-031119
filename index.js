4805695a7a54fea68d01b87a15fe3379020e7afe
Sachin Mahajan <sachinm78@gmail.com>
2019-06-12 16:16:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function findMatching (list, name) {
   });
 }
 
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
